574fd3081a4aaa576ae48e05af4defd9ea90587f
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-03-02 21:27:12 +0600

    Added bootstrap and others

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Connected!");
+const btnClickHandler = () => {
+  console.log("Btn Clicked!");
+};
+
+// Phone Search
+
+// URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
+
+// Example: https://openapi.programming-hero.com/api/phones?search=iphone
+// Phone detail url:
+
+// URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+// Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
