d97ff5e5da386cf8f90dbe88e74beb9af7300172
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-03-02 22:00:49 +0600

    Finish btn clickhandler function

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
+// Button click handler
 const btnClickHandler = () => {
   const input = document.getElementById("search-box");
-  const inputValue = input.value;
-  console.log(inputValue);
+  const inputValue = input.value.toLowerCase();
+  // Send url to API request
+  const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+  const searchResult = getSearchResult(url);
 };
-
-// Phone Search
-
-// URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
-
-// Example: https://openapi.programming-hero.com/api/phones?search=iphone
-// Phone detail url:
-
-// URL Format: https://openapi.programming-hero.com/api/phone/${id}
-
-// Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
