affe11f5798fdf33fbe784060d865c513f29c11e
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-03-02 22:03:58 +0600

    Successfully call the API

--- app.js
+++ app.js
@@ -4,5 +4,30 @@ const btnClickHandler = () => {
   const inputValue = input.value.toLowerCase();
   // Send url to API request
   const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
-  const searchResult = getSearchResult(url);
+  getSearchResult(url);
 };
+
+// Send API request
+const getSearchResult = (url) => {
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => updateUI(data))
+    .catch((e) => console.log(e));
+};
+
+// Update UI with the API Response
+const updateUI = (data) => {
+  console.log(data);
+};
+
+// Phone Search
+
+// URL Format:
+
+// Example: https://openapi.programming-hero.com/api/phones?search=iphone
+// Phone detail url:
+
+// URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+// Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
